c5fc5c4f74657c69c26e9a3c663052b38ec910a4
Zanepearton <z.pearton@accenture.com>
2023-02-22 21:23:22 +1100

    revised code

--- js/main.js
+++ js/main.js
@@ -1,185 +1,58 @@
-// // https://github.com/mrdoob/three.js/blob/dev/examples/misc_controls_orbit.html
-// //global variables
-// var scene, renderer, camera;
-// var cube;
-// var controls;
-// var geometry;
-// var light;
-// var material;
-// var particlecount = 20;
-// var verticecount = 20;
-// // var controls;
-// //add array of points
-// let  particles = [];
-// let  vertices = [];
-// let  colors = [];
-// //global variables
 
-// init();
-
-// function init(){
-
-//     scene = new THREE.Scene();
-//     camera = new THREE.PerspectiveCamera(28, window.innerWidth / window.innerHeight, 0.1, 3000);
-//     renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('myCanvas'), antialias: true });
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     document.body.appendChild(renderer.domElement);
-//     //let controls = new THREE.OrbitControls(camera, renderer.domElement);
-//     //controls.addEventListener('change',renderer);
-//     // light
-//     light = new THREE.AmbientLight(0xffffff, .08);
-//     scene.add(light);
-//     light = new THREE.PointLight(0xffffff, .5);
-//     light.position.set(0, 1, 2);
-//     scene.add(light);
-//     scene.add(cube);
-//     camera.position.z = 23;
-//     // initalise particles
-//     for (var i = 0; i < particlecount; i++) {
-//         //initalise array of points
-//         var x = THREE.MathUtils.randFloatSpread(6);
-//         var y = THREE.MathUtils.randFloatSpread(6);
-//         var z = THREE.MathUtils.randFloatSpread(6);
-//         //push random nunber to array list
-//         particles.push(x, y, z);
-//         for (var i = 0; i < verticecount; i++) {
-//             var x = THREE.MathUtils.randFloatSpread(6);
-//             var y = THREE.MathUtils.randFloatSpread(6);
-//             var z = THREE.MathUtils.randFloatSpread(6);
-//             vertices.push(x, y, z);
-//         }
-//         //console log the array initalial array
-//     }
-// }
-
-// //run main
-// init();
-
-// //load geo to buffer
-// function buffer(){
-
-// }
-
-// //add array geometry buffer
-// geometry = new THREE.BufferGeometry();
-// // geometry = new THREE.BufferGeometry().setFromPoints( points );
-// geometry.setAttribute('position', new THREE.Float32BufferAttribute(particles, 3));
-// geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-// // geometry.setAttribute('position', new THREE.Float32BufferAttribute(particles, 3));
-// var material = new THREE.PointsMaterial({ size: .05, color: 0xffffff });
-// // var material = new THREE.PointsMaterial({ size: 15, vertexColors: true });
-// var points = new THREE.Points(geometry, material);
-// var line = new THREE.Line( geometry, material );
-// scene.add(points);
-// scene.add(line);
-
-// //animate frame
-// function animate(){
-//     requestAnimationFrame(animate);
-//     //controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
-//     var rotation = .0009;
-//     // line.rotation.x += rotation;
-//     // line.rotation.y += rotation;
-//     // line.rotation.z += rotation; 
-//     points.rotation.x += rotation;
-//     points.rotation.y += rotation;
-//     points.rotation.z += rotation;
-//     console.log (points.rotation.x, points.rotation.y ,points.rotation.z);
-
-//     // console.log(points);
-// 	//render geometry 
-// 	render();
-
-// }
-
-// function render(){
-// 	renderer.render(scene,camera); 
-
-// }
-
-// function onWindowResize(){
-// 	// Camera frustum aspect ratio
-// 	camera.aspect = window.innerWidth / window.innerHeight;
-// 	// After making changes to aspect
-// 	camera.updateProjectionMatrix();
-// 	// Reset size
-// 	renderer.setSize(window.innerWidth, window.innerHeight);
-// 	}
-
-// window.addEventListener('resize', onWindowResize, false);
-// animate();
-
-
-
-
-
-
-
-
-
-
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-
-
-//frame
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// camera
-camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-camera.position.set( 400, 200, 0 );
-
-//controls 
-// controls = new OrbitControls(camera, renderer.domElement);
-// controls.listenToKeyEvents(window);
-// controls.enableDamping = true; 
-// controls.dampingFactor = 0.05;
-// controls.screenSpacePanning = false;
-// controls.minDistance = 100;
-// controls.maxDistance = 500;
-// controls.maxPolarAngle = Math.PI / 2;
+// import * as THREE from "three"
+//scene
+const scene = new THREE.Scene();
+//canvas size
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 
 // geometry 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({ color: "#0x00ff83" });
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
 
-camera.position.z = 5;
+//light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(10,1,10);
+scene.add(light);
 
-var animate = function () {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+// camera
+// const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+const camera = new THREE.PerspectiveCamera( 45, size.width/size.height);
+camera.position.z = 10;
+scene.add(camera);
+
+//render
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(size.width,size.height);
+renderer.render(scene, camera);
+
+//update window
+window.addEventListener("resize", () => {
+  //update size
+  console.log(window.innerWidth);
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  camera.updateProjectionMatrix();
+  camera.aspect = size.width/size.height;
+  renderer.setSize(size.width,size.height);
+})
+
+//render loop
+const loop = () => {
+  // for (var i = 0; i < particlecount; i++) {
+  //   const x = THREE.MathUtils.randFloatSpread(6);
+  //   const y = THREE.MathUtils.randFloatSpread(6);
+  //   const z = THREE.MathUtils.randFloatSpread(6);
+  // }
+  mesh.position.z += 0.01;
+  mesh.position.y += 0.001;
+  mesh.position.z += 0.001;
   renderer.render(scene, camera);
-};
-
-animate();
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-var animate = function () {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-};
-
-animate();
+  window.requestAnimationFrame(loop);
+}
+loop();
