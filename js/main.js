0356cca3470819280292e414a4ef06c47a0cfa01
Zanepearton <z.pearton@accenture.com>
2023-02-25 15:16:54 +1100

    revised screenwidth

--- js/main.js
+++ js/main.js
@@ -12,51 +12,55 @@ const size = {
 
 //globals
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
 const material = new THREE.MeshStandardMaterial({ color: "#0x00ff83" });
 const mesh = new THREE.Mesh(geometry,material);
-const light = new THREE.PointLight(0xffffff, 1, 100);
+const light = new THREE.PointLight(0xffffff, 1.3, 100);
 const camera = new THREE.PerspectiveCamera( 45, size.width/size.height);
 const canvas = document.querySelector('.webgl');
-const controls = new OrbitControls(camera, canvas);
+const controls = new OrbitControls(camera, canvas); //enable_cam
 const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
-const hello = "working yo"; 
-const rotation = .0009;
-var particlecount = 2000;
+var particlecount = 150000;
 
 //array
-let  particles = [];
+const  particles = [];
+const pointgeometry = new THREE.BufferGeometry();
 
-var bgeometry = new THREE.BufferGeometry();
-var bmaterial = new THREE.PointsMaterial({ size: .01, color: 0xffffff });
-var points = new THREE.Points(bgeometry, bmaterial);
+// const color3 = new THREE.Color("rgb(particles.x, particles.y, particles.z)");
+// console.log(color3);
+
+const pointmaterial = new THREE.PointsMaterial({ size: .002, color: 0x00ff83 });
+const points = new THREE.Points(pointgeometry, pointmaterial);
 
 // points.rotation.x += rotation;
 // points.rotation.y += rotation;
 // console.log (points.rotation.x, points.rotation.y ,points.rotation.z);
 
+
 //function_initMain
 function initMain(scene,mesh,light,camera,renderer,particlecount){
+    
+    scene.background = new THREE.Color( 0xffffff );
     pointcloud(particlecount);
-    bgeometry.setAttribute('position', new THREE.Float32BufferAttribute(particles, 3));
+    pointgeometry.setAttribute('position', new THREE.Float32BufferAttribute(particles, 3));
     scene.add(points);
     scene.add(mesh);
     scene.add(points);
     light.position.set(10,1,10);
     scene.add(light);
-    camera.position.z = 5;
+    camera.position.z = 1;
     scene.add(camera);
     renderer.setSize(size.width,size.height);
 }
 
-//function_pointcloud_write to an array
+//function_initalise_pointcloud_write_to_an_array
 function pointcloud(particlecount){
     for (var i = 0; i < particlecount; i++) {
         var x = THREE.MathUtils.randFloatSpread(6);
         var y = THREE.MathUtils.randFloatSpread(6);
         var z = THREE.MathUtils.randFloatSpread(6);
         particles.push(x, y, z);
-        console.log (x, y , z);
+        // console.log (x, y , z);
     }
 }
 
@@ -67,9 +71,12 @@ initMain(scene,mesh,light,camera,renderer,particlecount);
 function animate() {
     requestAnimationFrame(animate);
     const rotation = .0009;
+    //mesh_orbit
     mesh.rotation.x += rotation;
     mesh.rotation.y += rotation;
     mesh.rotation.z += rotation;
+    //point_orbit 
+    points.rotation.x += rotation;
     // console.log (mesh.rotation.x, mesh.rotation.y ,mesh.rotation.z);
     render();
 }
