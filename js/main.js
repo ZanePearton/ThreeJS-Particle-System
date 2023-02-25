1cd6d8ea4bca6c397142b91b39239da94c144f17
Zanepearton <z.pearton@accenture.com>
2023-02-25 11:27:08 +1100

    structure revised

--- js/main.js
+++ js/main.js
@@ -17,14 +17,17 @@ const light = new THREE.PointLight(0xffffff, 1, 100);
 const camera = new THREE.PerspectiveCamera( 45, size.width/size.height);
 const canvas = document.querySelector('.webgl');
 const controls = new OrbitControls(camera, canvas);
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 const hello = "working yo"; 
-// const rotation = .0009;
+const rotation = .0009;
+var particlecount = 20;
+//array
+let  particles = [];
 
-//functions
-function init(scene,mesh,light,camera,renderer){
-    console.log(hello)
-    scene.add(mesh)
+
+//functions_initMain
+function initMain(scene,mesh,light,camera,renderer,particlecount){
+    // pointcloud(particlecount);
     scene.add(mesh);
     light.position.set(10,1,10);
     scene.add(light);
@@ -32,7 +35,21 @@ function init(scene,mesh,light,camera,renderer){
     scene.add(camera);
     renderer.setSize(size.width,size.height);
 }
-init(scene,mesh,light,camera,renderer);
+
+//functions_pointcloud
+function pointcloud(particlecount){
+    const i = 0;
+    for (const i = 0; i < particlecount; i++) {
+        const x = THREE.MathUtils.randFloatSpread(6);
+        const y = THREE.MathUtils.randFloatSpread(6);
+        const z = THREE.MathUtils.randFloatSpread(6);
+        particles.push(x, y, z);
+        console.log (x, y , z);
+    }
+}
+
+//initialist_main_fuction
+initMain(scene,mesh,light,camera,renderer);
 
 //functions_animate
 function animate() {
@@ -41,7 +58,7 @@ function animate() {
     mesh.rotation.x += rotation;
     mesh.rotation.y += rotation;
     mesh.rotation.z += rotation;
-    console.log (mesh.rotation.x, mesh.rotation.y ,mesh.rotation.z);
+    // console.log (mesh.rotation.x, mesh.rotation.y ,mesh.rotation.z);
     render();
 }
 
@@ -52,15 +69,12 @@ function render() {
 
 //functions_animate
 function onWindowResize() {
-    // Camera frustum aspect ratio
     camera.aspect = window.innerWidth / window.innerHeight;
-    // After making changes to aspect
     camera.updateProjectionMatrix();
-    // Reset size
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 window.addEventListener('resize', onWindowResize, false);
-
+//initialist main fuction
 animate();
 
 
